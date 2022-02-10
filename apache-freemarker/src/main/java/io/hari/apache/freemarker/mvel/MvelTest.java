3b739ece8e7555836ca984d7e745f0c045d0987e
Hariom Yadav <yadav.hariom@flipkart.com>
2022-02-10 17:57:12 +0530

    updated

---
+++ apache-freemarker/src/main/java/io/hari/apache/freemarker/mvel/MvelTest.java
@@ -0,0 +1,36 @@
+package io.hari.apache.freemarker.mvel;
+
+import org.mvel2.MVEL;
+
+import java.util.*;
+
+public class MvelTest {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        list.add("arpit");
+        list.add("arpit2");
+
+//        skip_rest = false;
+//        for(item: collection) {
+//            if (!skip_rest) {
+//                /* do something */
+//                if (some condition) {
+//                    /* break by skipping */
+//                    skip_rest = true;
+//                }
+//            }
+//        }
+
+        System.out.println("-----");
+        for (int i =0; i < 10; i++) {System.out.println(i);}
+
+        for (String i : list) {System.out.println(i);}
+
+        System.out.println("-----");
+        MVEL.eval("for (int i =0; i < 5; i++) {System.out.println(i);}", list);
+
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("list", list);
+        MVEL.eval("for ( i : list) {System.out.println(i);}", context);
+    }
+}
