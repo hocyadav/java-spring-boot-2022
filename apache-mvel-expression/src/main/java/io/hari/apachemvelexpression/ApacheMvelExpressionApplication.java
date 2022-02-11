3fadb5978d910670f272ef7c8649031856405192
Hariom Yadav <yadav.hariom@flipkart.com>
2022-02-11 09:00:59 +0530

    apache mvel initial commit

---
+++ apache-mvel-expression/src/main/java/io/hari/apachemvelexpression/ApacheMvelExpressionApplication.java
@@ -0,0 +1,13 @@
+package io.hari.apachemvelexpression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheMvelExpressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheMvelExpressionApplication.class, args);
+	}
+
+}
---
+++ apache-mvel-expression/src/test/java/io/hari/apachemvelexpression/ApacheMvelExpressionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.apachemvelexpression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheMvelExpressionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
